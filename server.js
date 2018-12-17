74eef14dea25fea8c8564f69cf1fa1225a48d2ef
Webgoal <webgoal@Webgoals-iMac.local>
2018-12-17 16:07:06 -0200

    Add express server

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const next = require('next')
 const routes = require('./routes')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
-const {createServer} = require('http')
-
+const express = require('express')
 app.prepare().then(() => {
-  createServer(handler).listen(3000)
+  express().use(handler).listen(3000)
 })
